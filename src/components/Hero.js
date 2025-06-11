25e769f1a3e4e56dc1fb46e6840ac1398c686080
Albert Oketch <albert@paygoenergy.co>
2025-06-11 06:38:31 +0000

    small screen technology section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,13 +24,21 @@ export default function Hero() {
           </Box>
         </Box>
       </Box>
-      <Box className="inset-0 z-2">
+      <Box className="inset-0 z-2 hidden sm:flex">
         <img className="absolute parallax__cloudrun" src="./parallax/google_cloud_run-icon.svg" style={{ top:'50%', left:'80%'}} />
         <img className="absolute parallax__psql" src="./parallax/postgresql-icon.svg" style={{ top:'60%', left:'72%' }} />
         <img className="absolute parallax__node" src="./parallax/nodejs-icon.svg" style={{ top:'70%', left:'80%' }} />
         <img className="absolute parallax__firebase" src="./parallax/firebase-icon.svg" style={{ top:'80%', left:'72%' }} />
         <img className="absolute parallax__mongodb" src="./parallax/mongodb-icon.svg" style={{ top:'80%', left:'88%' }} />
         <img className="absolute parallax__appengine" src="./parallax/google_appengine-icon.svg" style={{ top:'60%', left:'88%' }} />
+      </Box>
+      <Box className="inset-0 z-2 sm:hidden">
+        <img className="absolute parallax__cloudrun" src="./parallax/google_cloud_run-icon.svg" style={{ top:'50%', left:'66%'}} />
+        <img className="absolute parallax__psql" src="./parallax/postgresql-icon.svg" style={{ top:'60%', left:'50%' }} />
+        <img className="absolute parallax__node" src="./parallax/nodejs-icon.svg" style={{ top:'70%', left:'66%' }} />
+        <img className="absolute parallax__firebase" src="./parallax/firebase-icon.svg" style={{ top:'80%', left:'50%' }} />
+        <img className="absolute parallax__mongodb" src="./parallax/mongodb-icon.svg" style={{ top:'80%', left:'82%' }} />
+        <img className="absolute parallax__appengine" src="./parallax/google_appengine-icon.svg" style={{ top:'60%', left:'82%' }} />
       </Box>
     </section>
   );
